9ec6def362132f66915a9a945b7c6025c76c4a16
elmar8287 <L-mar@inbox.ru>
2021-09-15 13:40:09 +0400

    Create components folder and add Calculator js file

---
+++ src/components/Calculator.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Calculator = () => {
+  return (
+    <div>
+      <span>Result</span>
+      <section>
+        <button type="button">AC</button>
+        <button type="button">+/-</button>
+        <button type="button">%</button>
+        <button type="button">&#247;</button>
+        <button type="button">7</button>
+        <button type="button">8</button>
+        <button type="button">9</button>
+        <button type="button">x</button>
+        <button type="button">4</button>
+        <button type="button">5</button>
+        <button type="button">6</button>
+        <button type="button">-</button>
+        <button type="button">1</button>
+        <button type="button">2</button>
+        <button type="button">3</button>
+        <button type="button">+</button>
+        <button type="button">0</button>
+        <button type="button">.</button>
+        <button type="button">=</button>
+        <button type="button">0</button>
+      </section>
+    </div>
+  );
+};
+
+export default Calculator;
