4e2d03a78cc8540c9f67adda1f917ffe224976d4
elmar8287 <L-mar@inbox.ru>
2021-09-15 16:56:55 +0400

    Fix linter issues

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,28 +2,28 @@ import React from 'react';
 
 const Calculator = () => {
   return (
-    <div class="calculator">
+    <div className="calculator">
       <input type="number" />
-      <section class="buttons">
+      <section className="buttons">
         <button type="button">AC</button>
         <button type="button">+/-</button>
         <button type="button">%</button>
-        <button type="button" class="orange">&#247;</button>
+        <button type="button" className="orange">&#247;</button>
         <button type="button">7</button>
         <button type="button">8</button>
         <button type="button">9</button>
-        <button type="button" class="orange">x</button>
+        <button type="button" className="orange">x</button>
         <button type="button">4</button>
         <button type="button">5</button>
         <button type="button">6</button>
-        <button type="button" class="orange">-</button>
+        <button type="button" className="orange">-</button>
         <button type="button">1</button>
         <button type="button">2</button>
         <button type="button">3</button>
-        <button type="button" class="orange">+</button>
-        <button type="button" class="zero">0</button>
+        <button type="button" className="orange">+</button>
+        <button type="button" className="zero">0</button>
         <button type="button">.</button>
-        <button type="button" className="orange">=</button>
+        <button type="button" classNameName="orange">=</button>
       </section>
     </div>
   );
