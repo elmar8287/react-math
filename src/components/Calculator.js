927ee537247d5a7507f9849dd3e090bf88734687
elmar8287 <L-mar@inbox.ru>
2021-09-16 18:44:01 +0400

    Add onclick action for buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,50 +2,49 @@
 
 import React, { Component } from 'react';
 import calculate from '../logic/calculate';
-import operate from '../logic/operate';
 
 class Calculator extends Component {
   constructor(props) {
-    super();
-    this.state = {obj:{
-      total: null,
-      next: null,
-      operation: null,
-    },
-    result: '',  
-    }
+    super(props);
+    this.state = {};
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(e){
-    const result = calculate(this.state.obj, e.target.name, operate);
-    this.setState({obj: {...this.state.obj, ...result}});
-  }
+  handleClick = (buttonName) => {
+    this.setState((state) => calculate(state, buttonName));
+  };
+
+  showSolution = () => {
+    const { total, next, operation } = this.state;
+    const show = (total || '') + (operation || '') + (next || '');
+
+    return (show || '0');
+  };
   
   render() {
     return (
       <div className="calculator">
         <input type="number" />
         <section className="buttons">
-          <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
-          <button type="button" className="orange">&#247;</button>
-          <button type="button">7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
-          <button type="button" className="orange">x</button>
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
+          <button type="button" onClick={(ev) => this.handleClick(ev.target.name)}>AC</button>
+          <button type="button" onClick={(ev) => this.handleClick(ev.target.name)}>+/-</button>
+          <button type="button" onClick={(ev) => this.handleClick(ev.target.name)}>%</button>
+          <button type="button" className="orange" onClick={(ev) => this.handleClick(ev.target.name)}>&#247;</button>
+          <button type="button" onClick={(ev) => this.handleClick(ev.target.name)}>7</button>
+          <button type="button" onClick={(ev) => this.handleClick(ev.target.name)}>8</button>
+          <button type="button" onClick={(ev) => this.handleClick(ev.target.name)}>9</button>
+          <button type="button" className="orange" onClick={(ev) => this.handleClick(ev.target.name)}>x</button>
+          <button type="button" onClick={(ev) => this.handleClick(ev.target.name)}>4</button>
+          <button type="button" onClick={(ev) => this.handleClick(ev.target.name)}>5</button>
+          <button type="button" onClick={(ev) => this.handleClick(ev.target.name)}>6</button>
           <button type="button" className="orange">-</button>
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
-          <button type="button" className="orange">+</button>
-          <button type="button" className="zero">0</button>
-          <button type="button">.</button>
-          <button type="button" className="orange">=</button>
+          <button type="button" onClick={(ev) => this.handleClick(ev.target.name)}>1</button>
+          <button type="button" onClick={(ev) => this.handleClick(ev.target.name)}>2</button>
+          <button type="button" onClick={(ev) => this.handleClick(ev.target.name)}>3</button>
+          <button type="button" className="orange" onClick={(ev) => this.handleClick(ev.target.name)}>+</button>
+          <button type="button" className="zero" onClick={(ev) => this.handleClick(ev.target.name)}>0</button>
+          <button type="button" onClick={(ev) => this.handleClick(ev.target.name)}>.</button>
+          <button type="button" className="orange" onClick={(ev) => this.handleClick(ev.target.name)}>=</button>
         </section>
       </div>
     );
