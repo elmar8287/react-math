f87bf983fd3ddf84389eaa48cd8c7c61431465ac
elmar8287 <L-mar@inbox.ru>
2021-09-22 01:22:15 +0400

    Fix linter issues

--- src/pages/quotes.js
+++ src/pages/quotes.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
-
 const myStyle = {
   color: 'black',
-  margin: "30px",
+  margin: '30px',
 
 };
 
 const Quotes = () => (
-  <p style={ myStyle }>
-    Mathematics is not about numbers, equotions, computations, or algorithms: it is about 
+  <p style={myStyle}>
+    Mathematics is not about numbers, equotions, computations, or algorithms: it is about
     understanding. ~William Paul Thurston
   </p>
 );
