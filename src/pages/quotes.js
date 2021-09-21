f274bbbb654ca310bf5360cab02ee7fb545046e4
elmar8287 <L-mar@inbox.ru>
2021-09-22 00:31:06 +0400

    Modify Quotes page, add inline styling

---
+++ src/pages/quotes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const quotes = { margin: 20% }
+
+const Quotes = () => (
+  <blockquote style={ quotes }>
+    Mathematics is not about numbers, equotions, computations, or algorithms: it is about 
+    understanding. ~William Paul Thurston
+  </blockquote>
+);
+
+export default Quotes;
